09d8e2e1c68f7414df6f98a353494b46dc0d3e4b
Tclewis13 <the3rdi@gmail.com>
2024-09-01 20:28:29 -0400

    wrap done. TODO: Capitals, spaces, specials

--- odin_ruby_basics.rb
+++ odin_ruby_basics.rb
@@ -1,10 +1,18 @@
 def caesar_cipher(message, shift)
   charArray = message.bytes
-  charArray = charArray.map do 
-    |char| char + shift
+  charArray = charArray.map do |char| 
+    if char.between?(96,123)
+      if char + shift < 123
+        char + shift
+      else
+        remainder = 123 - char - shift
+        remainder = remainder.abs
+        char = 97 + remainder
+      end
+    end
   end
   charArray = charArray.map {|char| char.chr}
   cipherMessage = charArray.join
 end
 
-puts caesar_cipher('abcde', 1)
+puts caesar_cipher('wxyz', 3)
