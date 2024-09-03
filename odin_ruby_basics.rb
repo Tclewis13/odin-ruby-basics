9a33eb87ad736dae933d512e2fe3c984b1abffff
Tclewis13 <the3rdi@gmail.com>
2024-09-02 22:22:22 -0400

    rubocoped code

--- odin_ruby_basics.rb
+++ odin_ruby_basics.rb
@@ -1,43 +1,41 @@
+# frozen_string_literal: true
+
 def caesar_cipher(message, shift)
   charArray = message.bytes
-  charArray = charArray.map do |char| 
-    if char.between?(96,123)
+  charArray = charArray.map do |char|
+    if char.between?(96, 123)
       if char + shift < 123
         char + shift
       else
         remainder = 123 - char - shift
         remainder = remainder.abs
-        char = 97 + remainder
+        97 + remainder
       end
-    elsif char.between?(64,91)
+    elsif char.between?(64, 91)
       if char + shift < 91
         char + shift
       else
         remainder = 91 - char - shift
         remainder = remainder.abs
-        char = 65 + remainder
+        65 + remainder
       end
     else
       char
     end
   end
-  charArray = charArray.map {|char| char.chr}
-  cipherMessage = charArray.join
+  charArray = charArray.map(&:chr)
+  charArray.join
 end
 
 def dictionary_count(input_string, dictionary)
-  output_hash= Hash.new
-  dictionary.each {|word| word.downcase!}
-  input_string.downcase!
+  output_hash = {}
+  down_dictionary = dictionary.each(&:downcase)
+  down_string = input_string.downcase
 
-
-  input_string = input_string.downcase
-  dictionary.each do |word|
-    if input_string.scan(word).length > 0
-      output_hash[word] = (input_string.scan(word).length)
-    end
+  down_dictionary.each do |word|
+    output_hash[word] = down_string.scan(word).length if down_string.scan(word).length.positive?
   end
-  return output_hash
+  output_hash
 end
 
 def stock_picker(input_array)
@@ -46,44 +44,41 @@ def stock_picker(input_array)
   best_profit = 0
   input_array.each_with_index do |buy_price, buy_index|
     input_array.each_with_index do |sell_price, sell_index|
-      if sell_price - buy_price > best_profit && buy_index < sell_index
-        best_profit = sell_price - buy_price
-        best_buy = buy_index
-        best_sell = sell_index
-      end
+      next unless sell_price - buy_price > best_profit && buy_index < sell_index
+
+      best_profit = sell_price - buy_price
+      best_buy = buy_index
+      best_sell = sell_index
     end
   end
-  return [best_buy,best_sell]
+  [best_buy, best_sell]
 end
 
 def bubble_sort(input_array)
-  made_swap = true;
+  made_swap = true
 
-  while made_swap == true do
+  while made_swap == true
     made_swap = false
-    input_array.each_with_index do |number, index|
-      if !input_array[index + 1].nil? 
-        if input_array[index] > input_array[index + 1]
-          input_array[index], input_array[index + 1] = input_array[index + 1], input_array[index]
-          made_swap = true
-        end
+    input_array.each_with_index do |_number, index|
+      if !input_array[index + 1].nil? && input_array[index] > (input_array[index + 1])
+        input_array[index], input_array[index + 1] = input_array[index + 1], input_array[index]
+        made_swap = true
       end
     end
   end
 
-  return input_array
+  input_array
 end
 
+# dictionary_count test
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+puts dictionary_count("Howdy partner, sit down! How's it going?", dictionary)
 
-#dictionary_count test
-#dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-#puts dictionary_count("Howdy partner, sit down! How's it going?", dictionary)
+# caesar_cipher test
+puts caesar_cipher('What a string!', 5)
 
-#caesar_cipher test
-#puts caesar_cipher("What a string!", 5)
+# stockpicker test
+puts stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
 
-#stockpicker test
-#puts stock_picker([17,3,6,9,15,8,6,1,10])
-
-#bubble sort test
-puts bubble_sort([4,3,78,2,0,2])
+# bubble sort test
+puts bubble_sort([4, 3, 78, 2, 0, 2])
